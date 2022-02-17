e636beb41b69bd8958643624e08ee88a22b63513
Sebastiaan Blaas <sebastiaan@avido.nl>
2022-02-17 15:29:26 +0100

    Added consumption resource

---
+++ src/Resources/Account/Consumption.php
@@ -0,0 +1,15 @@
+<?php
+namespace Budgetlens\CopernicaRestApi\Resources\Account;
+
+use Budgetlens\CopernicaRestApi\Resources\BaseResource;
+
+class Consumption extends BaseResource
+{
+    public $emails;
+    public $texts;
+    public $fax;
+    public $webpages;
+    public $apicalls;
+    public $litmustests;
+    public $twofiftyoktests;
+}
