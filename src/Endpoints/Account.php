c0a0f12f70117d571e8cee1938d0f346600ddf42
Sebastiaan Blaas <sebastiaan@avido.nl>
2022-02-17 15:29:05 +0100

    Added consumption

--- src/Endpoints/Account.php
+++ src/Endpoints/Account.php
@@ -1,6 +1,7 @@
 <?php
 namespace Budgetlens\CopernicaRestApi\Endpoints;
 
+use Budgetlens\CopernicaRestApi\Resources\Account\Consumption;
 use Budgetlens\CopernicaRestApi\Resources\Account\Identity;
 
 class Account extends BaseEndpoint
@@ -10,7 +11,7 @@ class Account extends BaseEndpoint
      * @throws \Budgetlens\CopernicaRestApi\Exceptions\CopernicaApiException
      * @throws \Budgetlens\CopernicaRestApi\Exceptions\RateLimitException
      */
-    public function identity()
+    public function identity(): Identity
     {
         $response = $this->performApiCall(
             'GET',
@@ -20,4 +21,31 @@ class Account extends BaseEndpoint
         return new Identity(collect($response));
     }
 
+    /**
+     * @param \DateTime $begin
+     * @param \DateTime|null $end
+     * @return Consumption
+     * @throws \Budgetlens\CopernicaRestApi\Exceptions\CopernicaApiException
+     * @throws \Budgetlens\CopernicaRestApi\Exceptions\RateLimitException
+     */
+    public function consumption(\DateTime $begin, \DateTime $end = null)
+    {
+        $parameters = collect([
+            'begin' => $begin,
+            'end' => $end ?? new \DateTime(),
+        ])->reject(function ($value) {
+            return empty($value);
+        })->map(function ($value) {
+            return ($value instanceof \DateTime)
+                ? $value->format('Y-m-d')
+                : $value;
+        });
+
+        $response = $this->performApiCall(
+            'GET',
+            "consumption" . $this->buildQueryString($parameters->all())
+        );
+
+        return new Consumption(collect($response));
+    }
 }
