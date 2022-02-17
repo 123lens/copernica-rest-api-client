bead9f35c7f27934b629152db060c6bf07f8950c
Sebastiaan Blaas <sebastiaan@avido.nl>
2022-02-17 15:06:43 +0100

    Base test

--- tests/Feature/ClientTest.php
+++ tests/Feature/ClientTest.php
@@ -1,8 +1,9 @@
 <?php
-namespace Budgetlens\CopernicaRestApi\Tests;
+namespace Budgetlens\CopernicaRestApi\Tests\Feature;
 
 use Budgetlens\CopernicaRestApi\Client;
 use Budgetlens\CopernicaRestApi\Exceptions\CopernicaApiException;
+use Budgetlens\CopernicaRestApi\Tests\TestCase;
 
 class ClientTest extends TestCase
 {
@@ -10,7 +11,10 @@ class ClientTest extends TestCase
     public function performingAnHttpCallWithoutSettingAnAccessTokenThrowsAnException()
     {
         $this->expectException(CopernicaApiException::class);
+        $this->expectExceptionCode(400);
+        $this->expectExceptionMessage('Error executing API call : Copernica REST API Error 400: Invalid access token');
+
         $client = new Client('');
-        $client->performHttpCall('GET', 'non-existing-resource');
+        $client->account->identity();
     }
 }
