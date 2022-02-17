38e98ddd6d6e0bc5399909b3d49399d859fb683a
Sebastiaan Blaas <sebastiaan@avido.nl>
2022-02-17 16:53:07 +0100

    Added test

---
+++ tests/Feature/DatabaseTest.php
@@ -0,0 +1,38 @@
+<?php
+namespace Budgetlens\CopernicaRestApi\Tests\Feature\Endpoints;
+
+use Budgetlens\CopernicaRestApi\Resources\Account\Consumption;
+use Budgetlens\CopernicaRestApi\Resources\Account\Identity;
+use Budgetlens\CopernicaRestApi\Resources\Database;
+use Budgetlens\CopernicaRestApi\Resources\PaginatedResult;
+use Budgetlens\CopernicaRestApi\Tests\TestCase;
+use Illuminate\Support\Collection;
+
+class DatabaseTest extends TestCase
+{
+    /** @test */
+    public function listDatabases(): void
+    {
+        $this->useMock('200-get-list-databases.json');
+
+        $databases = $this->client->database->list();
+        $this->assertInstanceOf(PaginatedResult::class, $databases);
+        $this->assertIsInt($databases->start);
+        $this->assertIsInt($databases->limit);
+        $this->assertIsInt($databases->count);
+        $this->assertInstanceOf(Collection::class, $databases->data);
+        $this->assertCount(2, $databases->data);
+        $database = $databases->data->first();
+        $this->assertInstanceOf(Database::class, $database);
+        $this->assertIsInt($database->ID);
+        $this->assertSame(1, $database->ID);
+        $this->assertSame('PHPUnit Test Archived', $database->name);
+        $this->assertSame('Database 1', $database->description);
+        $this->assertTrue($database->archived);
+        $this->assertInstanceOf(\DateTime::class, $database->created);
+        $this->assertInstanceOf(PaginatedResult::class, $database->fields);
+        $this->assertInstanceOf(Collection::class, $database->fields->data);
+        $this->assertCount(2, $database->fields->data);
+        $this->assertInstanceOf(Database\Field::class, $database->fields->data->first());
+    }
+}
